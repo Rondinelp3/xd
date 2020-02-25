191b59f977855a62b420c07b56823d44cd9f8285
Rondinelp3 <daniel_rondinelp3@hotmail.com>
2020-02-25 15:40:12 -0500

    primer commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
+   $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+    $(document).ready(function () {
+        $('.slider').slider({
+
+            height : 1000, // default - height : 400
+            interval: 8000 // default - interval: 6000
+        });
+    });
+
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
